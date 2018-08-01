da8f4e9e734e2d3e300c4fffe1cbe870b1c2667f
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 15:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ class BoardMember {
   releasePressStatement() {return "You will see great things from Scuber."}
   sayHi() {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
+
+class Ceo
