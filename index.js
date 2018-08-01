1010753c4bff78e902f1fb6ff66d5a2e296d0544
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 15:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,9 @@ class BoardMember {
     this.training = training
   }
 
-  veto() {
-    return "No, I must disagree"
-  }
+  veto() {return "No, I must disagree"}
   approve() {return "You can do that!"}
   doCharity() {return "I like to help people."}
   releasePressStatement() {return "You will see great things from Scuber."}
-}
-
-BoardMember.prototype.releasePressStatement = function () {
-  return "You will see great things from Scuber."
-}
-
-BoardMember.prototype.sayHi = function () {
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  sayHi() {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
